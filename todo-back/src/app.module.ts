e544b6ef12fc69a148846237ab2fcd9bc6ee53a1
Oscar Ossa <oskrlex17@gmail.com>
2024-10-30 09:53:13 -0500

    feat(todo-back): configure AppModule for TypeORM with MySQL database

--- todo-back/src/app.module.ts
+++ todo-back/src/app.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserModule } from './adapter/user.module';
 
-@Module({})
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'todouser',
+      password: 'todopass',
+      database: 'todos',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+    UserModule,
+  ],
+})
 export class AppModule {}
